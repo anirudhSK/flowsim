f9a48f7b19a22e7c93f1e067e08c9c5eb2447071
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-07-08 06:55:30 +0000

    Compute tail stats

--- flowserver.cc
+++ flowserver.cc
@@ -1,6 +1,9 @@
 #include <cassert>
 #include <iostream>
 #include <limits>
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/stats.hpp>
+#include <boost/accumulators/statistics/tail_quantile.hpp>
 
 #include "flowserver.hh"
 
@@ -18,7 +21,9 @@ void FlowServer::tick( const double & tickno )
 {
   if ( next_event_time( tickno ) <= tickno ) {
     assert( not _flow_queue.empty() );
-    assert( tickno == next_event_time( tickno ) );
+    auto completed_flow = _flow_queue.front();
+    assert( tickno == completed_flow.get_departure() );
+    _fcts.push_back( tickno - completed_flow.get_creation_time() );
     _flow_queue.pop();
   }
 }
@@ -26,4 +31,16 @@ void FlowServer::tick( const double & tickno )
 double FlowServer::next_event_time( const double & tickno __attribute__ ((unused)) ) const
 {
   return _flow_queue.empty() ? numeric_limits<double>::max() :  _flow_queue.front().get_departure();
+}
+
+void FlowServer::output_stats( const double & quantile )
+{
+  boost::accumulators::accumulator_set< double,
+                                        boost::accumulators::stats
+                                        <boost::accumulators::tag::tail_quantile
+                                        <boost::accumulators::right> > > fct_acc
+                                      ( boost::accumulators::tag::tail<boost::accumulators::right>::cache_size = _fcts.size() );
+  for_each( _fcts.begin(), _fcts.end(), [&] ( const double & val ) { fct_acc( val ); } );
+  cout << " Total of " << _fcts.size() << " flows " << endl;
+  cout << quantile << " quantile " <<  boost::accumulators::quantile( fct_acc, boost::accumulators::quantile_probability = quantile ) << endl;
 }
--- flowsim.cc
+++ flowsim.cc
@@ -10,23 +10,25 @@ using namespace std;
 
 int main( int argc, const char* argv[] )
 {
-  if ( argc < 4 ) {
-    cout << "Usage: ./flowsim mean_on_duration (packets)  mean_off_duration (ms)  link_speed (packets/ms)  " << endl;
+  if ( argc < 6 ) {
+    cout << "Usage: ./flowsim mean_on_duration (packets)  mean_off_duration (ms)  link_speed (packets/ms) total_ticks (ms)  quantile (0--1) " << endl;
     exit( EXIT_SUCCESS );
   }
 
-  double mean_on_duration = stod( argv[ 1 ] );
-  double mean_off_duration = stod( argv[ 2 ] );
-  double link_speed = stod( argv[ 3 ] );
+  const double mean_on_duration = stod( argv[ 1 ] );
+  const double mean_off_duration = stod( argv[ 2 ] );
+  const double link_speed = stod( argv[ 3 ] );
+  const double total_ticks = stod( argv[ 4 ] );
+  const double quantile = stod( argv[ 5 ] );
 
   FlowGenerator flow_generator ( mean_on_duration, mean_off_duration, global_PRNG() );
   FlowServer flow_server( link_speed );
   double tickno = 0;
-  while ( tickno < 1000000 ) {
+  while ( tickno < total_ticks ) {
     tickno = min( flow_generator.next_event_time( tickno ),
                   flow_server.next_event_time( tickno ) );
     flow_generator.tick( flow_server, tickno );
     flow_server.tick( tickno );
   }
-  //flow_server.output_stats();
+  flow_server.output_stats( quantile );
 }
