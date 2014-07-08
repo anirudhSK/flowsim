f553f23b0b4419e69eb3fd51928df503bdfae32f
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-07-08 06:54:46 +0000

    Make more readable

--- flowserver.cc
+++ flowserver.cc
@@ -8,9 +8,9 @@ using namespace std;
 
 void FlowServer::accept( const Flow & s_flow )
 {
-  double current_departure =  _flow_queue.empty() ? s_flow.get_departure() :  _flow_queue.back().get_departure();
+  double current_departure =  _flow_queue.empty() ? s_flow.get_creation_time() :  _flow_queue.back().get_departure();
   Flow new_flow( s_flow );
-  new_flow.set_departure( current_departure + s_flow.get_remaining_flow_size() / _link_speed ); 
+  new_flow.set_departure( current_departure + new_flow.get_remaining_flow_size() / _link_speed );
   _flow_queue.emplace( new_flow );
 }
 
