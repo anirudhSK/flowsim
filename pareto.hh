0c99f17afdc676bb09078fac4e20150e4ff90e4c
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-07-09 12:15:34 -0700

    Class to abstract Pareto distributions.

---
+++ pareto.hh
@@ -0,0 +1,31 @@
+#ifndef PARETO_HH
+#define PARETO_HH
+
+#include <boost/random/uniform_real_distribution.hpp>
+
+#include "random.hh"
+
+class Pareto
+{
+private:
+  boost::random::uniform_real_distribution<> distribution;
+
+  PRNG & prng;
+
+  double _shape;
+  double _scale;
+
+public:
+  /* shape = 2 for convenience */
+  /* mean = 2 * scale / ( 2 - 1) = 2 * scale */
+  /* scale = mean / 2.0 */ 
+  Pareto( const double & mean, PRNG & s_prng )
+   : distribution( 0, 1.0 ),
+     prng( s_prng ),
+     _shape( 2.0 ),
+     _scale( mean / 2.0 ) {}
+  
+  double sample( void ) { return _scale / pow( distribution( prng ), 1 / _shape ); }
+};
+
+#endif
