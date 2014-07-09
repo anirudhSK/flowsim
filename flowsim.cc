7bc9af5724b4b3a0ca1426cc0d02d3eba78c34e9
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-07-09 16:12:28 -0700

    Revert "Simplify: Use normalized mu = 1, and lambda = ... to compare against tables."
    This reverts commit ce634948dcf0998b861e0ffe5db107bff5b07967.

--- flowsim.cc
+++ flowsim.cc
@@ -11,20 +11,21 @@ using namespace std;
 
 int main( int argc, const char* argv[] )
 {
-  if ( argc < 4 ) {
-    cout << "Usage: ./flowsim lambda(0--1)  total_ticks (ms)  sch_type [ quantile (0--1) ] " << endl;
+  if ( argc < 6 ) {
+    cout << "Usage: ./flowsim mean_on_duration (packets)  mean_off_duration (ms)  link_speed (packets/ms) total_ticks (ms)  sch_type [quantile (0 -- 1)] " << endl;
     exit( EXIT_SUCCESS );
   }
 
-  const double lambda = stod( argv[ 1 ] );
-  assert( lambda > 0 and lambda < 1 );
-  const double total_ticks = stod( argv[ 2 ] );
-  const string sch_type ( argv[ 3 ] );
-  const double quantile = ( argc >= 5 ) ? stod( argv[ 4 ] ) : 0.0;
+  const double mean_on_duration = stod( argv[ 1 ] );
+  const double mean_off_duration = stod( argv[ 2 ] );
+  const double link_speed = stod( argv[ 3 ] );
+  const double total_ticks = stod( argv[ 4 ] );
+  const string sch_type ( argv[ 5 ] );
+  const double quantile = ( argc >= 7 ) ? stod( argv[ 6 ] ) : 0.0;
 
-  FlowGenerator flow_generator ( 1.0, 1.0 / lambda, global_PRNG() );
+  FlowGenerator flow_generator ( mean_on_duration, mean_off_duration, global_PRNG() );
   if ( sch_type == "srpt" ) {
-    SrptServer srpt_server( 1.0 );
+    SrptServer srpt_server( link_speed );
     double tickno = 0;
     while ( tickno < total_ticks ) {
       tickno = min( flow_generator.next_event_time( tickno ),
@@ -35,7 +36,7 @@ int main( int argc, const char* argv[] )
     }
     ( quantile != 0 ) ? srpt_server.output_stats( quantile ) : srpt_server.output_average();
   } else {
-    FlowServer fcfs_server( 1.0 );
+    FlowServer fcfs_server( link_speed );
     double tickno = 0;
     while ( tickno < total_ticks ) {
       tickno = min( flow_generator.next_event_time( tickno ),
