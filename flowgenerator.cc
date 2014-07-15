bb843171120c5ad16ab74a378254f687bcf12440
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-07-15 16:33:26 -0700

    dist_type is only for on durations, off times are always exponential
    Change-Id: I679dcbf2ee52dbc4c2b768c74daf1dbd796bd3fe

--- flowgenerator.cc
+++ flowgenerator.cc
@@ -5,7 +5,7 @@ FlowGenerator::FlowGenerator( const double & mean_on_duration,
                               const double & mean_off_duration,
                               const DistType & dist_type,
                               PRNG & s_prng )
-  : _start_distribution( get_dist( dist_type, mean_off_duration, s_prng ) ),
+  : _start_distribution( get_dist( DistType::EXPONENTIAL, mean_off_duration, s_prng ) ),
     _stop_distribution( get_dist( dist_type, mean_on_duration, s_prng ) ),
     _next_flow_start( _start_distribution->sample() )
 {}
